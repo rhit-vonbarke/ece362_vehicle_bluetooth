9b906569c7d9332db30aff7142ccee93c3eb1165
mannanej <45293726+mannanej@users.noreply.github.com>
2022-01-26 14:38:54 -0500

    Combined Eddie and Kurtis code

--- Robot_Bluetooth/src/Main2.java
+++ Robot_Bluetooth/src/Main2.java
@@ -2,9 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -17,12 +17,10 @@ import org.jfree.data.xy.XYSeriesCollection;
 import com.fazecast.jSerialComm.SerialPort;
 
 public class Main2 {
-	
+
 	public static final byte[] BT_NEWLINE = new byte[] { (byte) 0x0A };
 	static SerialPort chosenPort;
 	static int x = 0;
-	
-	
 
 	public static void main(String[] args) {
 
@@ -54,7 +52,7 @@ public class Main2 {
 		JFreeChart chart = ChartFactory.createXYLineChart("Current Readings", "Time (seconds)", "Current (mA)",
 				dataset);
 		window.add(new ChartPanel(chart), BorderLayout.CENTER);
-		
+
 		// configure the connect button and use another thread to listen for data
 		startButton.addActionListener(new ActionListener() {
 			@Override
@@ -69,11 +67,11 @@ public class Main2 {
 							System.out.println("Port is Open");
 							startButton.setText("Disconnect");
 							portList.setEnabled(false);
-//							try {
-//								TimeUnit.SECONDS.wait(10);
-//							} catch (InterruptedException e) {
-//								e.printStackTrace();
-//							}
+//									try {
+//										TimeUnit.SECONDS.wait(10);
+//									} catch (InterruptedException e) {
+//										e.printStackTrace();
+//									}
 							while (true) {
 								try {
 									if (System.in.read() == 83) { // If the console gets an "S"
@@ -96,27 +94,38 @@ public class Main2 {
 					Thread thread = new Thread() {
 						@Override
 						public void run() {
-							System.out.println("Made it to Run");
-							chosenPort.writeBytes(BT_NEWLINE, 1);
-							Scanner scanner = new Scanner(chosenPort.getInputStream());
-							while (scanner.hasNextLine()) {
-								System.out.println("Has Next Line");
+							while (true) {
+								System.out.println("Made it to Run");
+								chosenPort.writeBytes(BT_NEWLINE, 1);
 								try {
-									String line = scanner.nextLine();
-									System.out.println(line);
-									// int number = Integer.parseInt(line);
-									// series.add(x++, 1023 - number);
-									VehicleDataPoint data = processInput(line, series);
-									series.add(x++, data.current);
-									window.repaint();
 									TimeUnit.SECONDS.sleep(1);
-									chosenPort.writeBytes(BT_NEWLINE, 1);
-								} catch (Exception e) {
+								} catch (InterruptedException e) {
 									e.printStackTrace();
 								}
+								InputStream in = chosenPort.getInputStream();
+								StringBuilder builder = new StringBuilder();
+								char value = ' ';
+								for (int i = 0; i < 100; i++) {
+									try {
+										value = (char) in.read();
+									} catch (IOException e1) {
+										e1.printStackTrace();
+									}
+									builder.append(value);
+									if (value == '*') {
+										break;
+									}
+								}
+								System.out.println(builder.toString());
+								// System.out.println("Has Next Line");
+								// System.out.println(line);
+								// int number = Integer.parseInt(line);
+								// series.add(x++, 1023 - number);
+								series.add(x++, 1023 - (Math.random() * 10));
+								window.repaint();
+								// chosenPort.writeBytes(BT_NEWLINE, 1);
+								// System.out.println("Closing");
 							}
-							System.out.println("Closing");
-							scanner.close();
 						}
 					};
 					thread.start();
@@ -133,19 +142,19 @@ public class Main2 {
 		// show the window
 		window.setVisible(true);
 	}
-	
+
 	public static VehicleDataPoint processInput(String input, XYSeries series) {
-		switch(input.charAt(0)) {
-		case '#': //data
+		switch (input.charAt(0)) {
+		case '#': // data
 			String[] dataSeg = input.split(":");
-			String milliStr = dataSeg[dataSeg.length-1];
-			int millis = Integer.valueOf(milliStr.substring(0,milliStr.length()-1));
+			String milliStr = dataSeg[dataSeg.length - 1];
+			int millis = Integer.valueOf(milliStr.substring(0, milliStr.length() - 1));
 			return new VehicleDataPoint(dataSeg, millis);
 		default:
 			return null;
-		}		
+		}
 	}
-	
+
 	public static class VehicleDataPoint {
 		public double shuntVoltage;
 		public double busVoltage;
@@ -153,7 +162,7 @@ public class Main2 {
 		public double power;
 		public double loadVoltage;
 		public int millis;
-		
+
 		public VehicleDataPoint(String[] data, int millis) {
 			this.shuntVoltage = Double.valueOf(data[0]);
 			this.busVoltage = Double.valueOf(data[1]);
@@ -162,7 +171,7 @@ public class Main2 {
 			this.loadVoltage = Double.valueOf(data[4]);
 			this.millis = millis;
 		}
-		
+
 	}
-	
+
 }
